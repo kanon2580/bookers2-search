4837e7000a9f43acc5d759f04c9c6184cd297757
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-09 20:31:40 +0900

    [update] 変数名変更

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,24 +5,24 @@ class SearchController < ApplicationController
 
     if params[:genre] == "user"
       if params[:direction] == "完全一致"
-        @users = User.where(name: "#{params[:word]}")
+        @users = User.where(name: "#{params[:q]}")
       elsif params[:direction] == "前方一致"
-        @users = User.where('name LIKE ?', "#{params[:word]}%")
+        @users = User.where('name LIKE ?', "#{params[:q]}%")
       elsif params[:direction] == "後方一致"
-        @users = User.where('name LIKE ?', "%#{params[:word]}")
+        @users = User.where('name LIKE ?', "%#{params[:q]}")
       elsif params[:direction] == "部分一致"
-        @users = User.where('name LIKE ?', "%#{params[:word]}%")
+        @users = User.where('name LIKE ?', "%#{params[:q]}%")
       end
 
     elsif params[:genre] == "book"
       if params[:direction] == "完全一致"
-        @books = Book.where(title: "#{params[:word]}", body: "#{params[:word]}")
+        @books = Book.where(title: "#{params[:q]}", body: "#{params[:q]}")
       elsif params[:direction] == "前方一致"
-        @books = Book.where('title LIKE ? OR body LIKE ?', "#{params[:word]}%", "#{params[:word]}%")
+        @books = Book.where('title LIKE ? OR body LIKE ?', "#{params[:q]}%", "#{params[:q]}%")
       elsif params[:direction] == "後方一致"
-        @books = Book.where('title LIKE ? OR body LIKE ?', "%#{params[:word]}", "#{params[:word]}%")
+        @books = Book.where('title LIKE ? OR body LIKE ?', "%#{params[:q]}", "#{params[:q]}%")
       elsif params[:direction] == "部分一致"
-        @books = Book.where('title LIKE ? OR body LIKE ?', "%#{params[:word]}%", "%#{params[:word]}%")
+        @books = Book.where('title LIKE ? OR body LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%")
       end
     end
   end
